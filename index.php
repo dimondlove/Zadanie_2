164b352f4abaf6fadc1bba0b8a8aef7ad2898d12
Yuriy_Rudnev <rudnev2020@gmail.com>
2021-10-18 21:12:36 +0300

    Поправил логику + подключил Bootstrap5

--- index.php
+++ index.php
@@ -3,23 +3,40 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
 	<title>Калькулятор</title>
 </head>
 <body>
-	<h1>Калькулятор</h1>
-	<h2>Введите числа и выберите операцию</h2>
-	<form action="" method="post">
-		<input type="text" name="number_1" placeholder="Первое число">
-		<select name="operation">
-			<option value="plus">+</option>
-			<option value="minus">-</option>
-			<option value="multiply">*</option>
-			<option value="divide">/</option>
-		</select>
-		<input type="text" name="number_2" placeholder="Второе число">
+	<div class="container-fluid text-center">
+		<h1>Калькулятор</h1>
+		<h2>Введите числа и выберите операцию</h2>
+		<form action="" method="post" class="row g-3 justify-content-center">
+			<div class="col-12 col-lg-3">
+				<input type="text" class="form-control" name="number_1" placeholder="Первое число">
+			</div>
 
-		<input type="submit" name="submit" value="Получить ответ">
-	</form>
+			<div class="col-4 col-lg-2">
+				<select name="operation" class="form-select">
+					<option value="plus">+</option>
+					<option value="minus">-</option>
+					<option value="multiply">*</option>
+					<option value="divide">/</option>
+				</select>
+			</div>
+
+			<div class="col-12 col-lg-3">
+				<input type="text" class="form-control" name="number_2" placeholder="Второе число">
+			</div>
+
+			<div class="col-12">
+				<button type="submit" class="btn btn-primary" name="submit">Получить ответ</button>
+			</div>
+		</form>
+	</div>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
 </body>
 </html>
 
@@ -33,7 +50,7 @@
 
 		// Проверяем все ли поля заполнены
 		if (!$operation || (!$number_1 && $number_1 != '0') || (!$number_2 && $number_2 != '0')) {
-			$error = "Вы заполнили не все поля";
+			$error = 'Вы заполнили не все поля';
 		}
 		else {
 
@@ -68,14 +85,17 @@
 				}
 			}
 
-			if(isset($error)) {
-				echo "Ошибка: $error";
-			}
-			else {
-				echo "Ответ: $result";
-			}
+		}
 
-
+		if(isset($error)) {
+			echo "<div class='container-fluid text-center'>";
+			echo "<div class='text-danger'><h3>Ошибка: $error</h3></div>";
+			echo "</div>";
+		}
+		else {
+			echo "<div class='container-fluid text-center'>";
+			echo "<div class='text-success'><h3>Ответ: $result</h3></div>";
+			echo "</div>";
 		}
 
 	}
